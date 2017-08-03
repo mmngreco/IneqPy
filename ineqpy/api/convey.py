67cdb2ed63bb8036ddf0d757e6630903f9ad422c
mmngreco <mmngreco@gmail.com>
2017-08-03 21:53:43 +0200

    Updated api class

--- ineqpy/api/convey.py
+++ ineqpy/api/convey.py
@@ -2,29 +2,28 @@ import pandas as pd
 from .. import statistics
 from .. import inequality
 from functools import partial
+from types import MethodType
+import inspect
 
 
-class Survey(pd.DataFrame):
+def _attach_method(module, instance):
+    # get methods names contained in module
+    method_name_list = inspect.getmembers(module, inspect.isfunction)
+    for method_name, func in method_name_list:
+        # if method_name.startswith('_'): continue  # avoid private methods
+        func = getattr(module, method_name)  # get function
+        if 'weights' in inspect.signature(func).parameters:  # replace weights variable
+            func = partial(func, weights=instance.weights)
+        func = MethodType(func, instance)
+        setattr(instance, method_name, func)
 
-    def __init__(self, data, index, columns, weights=None, group=None):
-        super().__init__(data, index, columns)
+
+class Survey(object):
+
+    def __init__(self, data, index=None, columns=None, weights=None, group=None):
+        self.data = pd.DataFrame(data=data, index=index, columns=columns)
         self.weights = weights
         self.group = group
-
-
-class Convey(Survey):
-
-    @classmethod
-    def wrapper(cls):
-        for method in dir(statistics):
-            if method.startswith('_'): continue
-            if hasattr(getattr(my_module, method), 'weights'):
-                func = partial(getattr(my_module, method), weights=self.weights)
-            setattr(cls, method, func)
-
-        for method in dir(inequality):
-            if method.startswith('_'): continue
-            if hasattr(getattr(my_module, method), 'weights'):
-                func = partial(getattr(my_module, method), weights=self.weights)
-            setattr(cls, method, func)
+        _attach_method(statistics, self)
+        _attach_method(inequality, self)
 
