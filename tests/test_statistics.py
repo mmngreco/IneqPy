771a566dcfcc2d7e6dbf3a7e541499caeca42c02
mmngreco <mmngreco@gmail.com>
2017-08-08 22:24:36 +0200

    Fixed test_statistics

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -19,7 +19,6 @@ class TestStatistics(unittest.TestCase):
                    '\nrepeated_x = {}' \
                    '\nx = {}' \
                    '\nw = {}'.format(i, str(repeated_x), str(x), str(w))
-            print(mssg)
             return mssg
 
     def test_statistics(self):
@@ -61,7 +60,7 @@ class TestStatistics(unittest.TestCase):
 
             with self.subTest(name='percentile', i=i):
                 p = 50
-                real = np.percentile(repeated_x, p, interpolation='midpoint')
+                real = np.percentile(repeated_x, p, interpolation='lower')
                 obtained = _statistics.percentile(x,w, percentile=p)
                 mssg = self.print_values_when_error(real, obtained, i, repeated_x, x, w)
                 nptest.assert_almost_equal(
