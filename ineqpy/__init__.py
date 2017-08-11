b34aff3861cdff1020e8992ad10db54607547eb0
mmngreco <mmngreco@gmail.com>
2017-08-11 22:41:57 +0200

    added versioneer to init file

--- ineqpy/__init__.py
+++ ineqpy/__init__.py
@@ -5,13 +5,12 @@ from .inequality import *
 from .statistics import *
 from . import api
 from . import utils
+from ._version import get_versions
 
+__version__ = get_versions()['version']
 __author__ = "Maximiliano Greco"
-__version__ = "0.0.2"
 __maintainer__ = "Maximiliano Greco"
 __email__ = "mmngreco@gmail.com"
 __status__ = "Production"
 
-from ._version import get_versions
-__version__ = get_versions()['version']
 del get_versions
