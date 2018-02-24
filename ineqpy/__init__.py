c7c6a1ca1e57278c66409fb95377eb019d634b7d
Maximiliano Greco <mmngreco@gmail.com>
2018-02-24 23:48:19 +0100

    convert from relative to absolute imports

--- ineqpy/__init__.py
+++ ineqpy/__init__.py
@@ -2,14 +2,14 @@
 IneqPy: A python package for inequality analysis.
 '''
 
-from .inequality import *
-from . import inequality
-from .statistics import *
-from . import statistics
-from . import grouped
-from . import api
-from . import utils
-from ._version import get_versions
+from ineqpy.inequality import *
+from ineqpy import inequality
+from ineqpy.statistics import *
+from ineqpy import statistics
+from ineqpy import grouped
+from ineqpy import api
+from ineqpy import utils
+from ineqpy._version import get_versions
 
 __version__ = get_versions()['version']
 __author__ = "Maximiliano Greco"
