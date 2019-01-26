e68b0e42ed93bdc29cd20a391a5eff31c8fba4c7
Maximiliano Greco <mgreco@ets.es>
2019-01-26 17:58:05 +0100

    TST: test api using pytest

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,21 +1,22 @@
 import numpy as np
 import ineqpy
 
-svy = ineqpy.api.Survey
-data = np.random.randint(0,100, (int(1e3),3))
-w = np.random.randint(1,10, int(1e3))
-data = np.c_[data, w]
-columns = list('abcw')
+def test_api():
+    # todo improve this test.
+    # only checks that all methods works.
+    svy = ineqpy.api.Survey
+    data = np.random.randint(0,100, (int(1e3),3))
+    w = np.random.randint(1,10, int(1e3))
+    data = np.c_[data, w]
+    columns = list('abcw')
 
-df = svy(data=data, columns=columns, weights='w')
-
-df
-df.weights
-df.mean('a')
-df.var('a')
-df.skew('a')
-df.kurt('a')
-df.gini('a')
-df.atkinson('a')
-df.theil('a')
-df.percentile('a')
+    df = svy(data=data, columns=columns, weights='w')
+    df.weights
+    df.mean('a')
+    df.var('a')
+    df.skew('a')
+    df.kurt('a')
+    df.gini('a')
+    df.atkinson('a')
+    df.theil('a')
+    df.percentile('a')
