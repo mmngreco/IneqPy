041b14cfc8ec035d3bce6934c3268309dcea81a6
Maximiliano Greco <mgreco@ets.es>
2019-01-26 18:04:20 +0100

    CLN: Cleanup code

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,22 +1,23 @@
 import numpy as np
 import ineqpy
 
+
 def test_api():
     # todo improve this test.
     # only checks that all methods works.
     svy = ineqpy.api.Survey
-    data = np.random.randint(0,100, (int(1e3),3))
-    w = np.random.randint(1,10, int(1e3))
+    data = np.random.randint(0, 100, (int(1e3), 3))
+    w = np.random.randint(1, 10, int(1e3))
     data = np.c_[data, w]
-    columns = list('abcw')
+    columns = list("abcw")
 
-    df = svy(data=data, columns=columns, weights='w')
+    df = svy(data=data, columns=columns, weights="w")
     df.weights
-    df.mean('a')
-    df.var('a')
-    df.skew('a')
-    df.kurt('a')
-    df.gini('a')
-    df.atkinson('a')
-    df.theil('a')
-    df.percentile('a')
+    df.mean("a")
+    df.var("a")
+    df.skew("a")
+    df.kurt("a")
+    df.gini("a")
+    df.atkinson("a")
+    df.theil("a")
+    df.percentile("a")
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -13,57 +13,55 @@ def gen_inputs(n_tuples=100):
         # repeated_w is a vector of ones.
         yield x, w, repeated_x
 
+
 INPUTS = gen_inputs()
 
 
 @pytest.mark.parametrize("x,w,r_x", INPUTS)
 def test_mean(x, w, r_x):
-            real = np.mean(r_x)
-            obtained = _statistics.mean(x, w)
-            nptest.assert_almost_equal(obtained, real)
+    real = np.mean(r_x)
+    obtained = _statistics.mean(x, w)
+    nptest.assert_almost_equal(obtained, real)
 
 
 @pytest.mark.parametrize("x,w,r_x", INPUTS)
 def test_variance(x, w, r_x):
-            real = np.var(r_x)
-            obtained = _statistics.var(x, w)
-            nptest.assert_almost_equal(obtained, real)
+    real = np.var(r_x)
+    obtained = _statistics.var(x, w)
+    nptest.assert_almost_equal(obtained, real)
 
 
 @pytest.mark.parametrize("x,w,r_x", INPUTS)
 def test_kurt(x, w, r_x):
-            real = sc.kurtosis(r_x) + 3
-            obtained = _statistics.kurt(x, w)
-            nptest.assert_almost_equal(obtained, real)
+    real = sc.kurtosis(r_x) + 3
+    obtained = _statistics.kurt(x, w)
+    nptest.assert_almost_equal(obtained, real)
 
 
 @pytest.mark.parametrize("x,w,r_x", INPUTS)
 def test_skew(x, w, r_x):
-            real = sc.skew(r_x)
-            obtained = _statistics.skew(x, w)
-            nptest.assert_almost_equal(obtained, real)
+    real = sc.skew(r_x)
+    obtained = _statistics.skew(x, w)
+    nptest.assert_almost_equal(obtained, real)
 
 
 @pytest.mark.parametrize("x,w,r_x", INPUTS)
 def test_coef_variation(x, w, r_x):
-            real = np.var(r_x) ** 0.5 / abs(np.mean(r_x))
-            obtained = _statistics.coef_variation(x,w)
-            nptest.assert_almost_equal(obtained, real)
-
+    real = np.var(r_x) ** 0.5 / abs(np.mean(r_x))
+    obtained = _statistics.coef_variation(x, w)
+    nptest.assert_almost_equal(obtained, real)
 
 
 @pytest.mark.parametrize("x,w,r_x", INPUTS)
 def test_percentile(x, w, r_x):
-            p = 50
-            real = np.percentile(r_x, p, interpolation='lower')
-            obtained = _statistics.percentile(x,w, percentile=p)
-            mssg = msg_assert(real, obtained, r_x, x, w)
-            nptest.assert_almost_equal(obtained, real, mssg)
+    p = 50
+    real = np.percentile(r_x, p, interpolation="lower")
+    obtained = _statistics.percentile(x, w, percentile=p)
+    mssg = msg_assert(real, obtained, r_x, x, w)
+    nptest.assert_almost_equal(obtained, real, mssg)
+
 
 def msg_assert(real, obtained, r_x, x, w):
     if abs(real - obtained) > 1e-6:
-        mssg = '\nr_x = {}' \
-               '\nx = {}' \
-               '\nw = {}'.format(str(r_x), str(x), str(w))
+        mssg = "\nr_x = {}\nx = {}\nw = {}".format(str(r_x), str(x), str(w))
         return mssg
-
