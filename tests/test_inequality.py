2403fcb00eb2cb12ad56f166db7a5e6eafdea6de
David Davó <david@ddavo.me>
2022-02-11 13:01:52 +0100

    Finished ratio 10:90

--- tests/test_inequality.py
+++ tests/test_inequality.py
@@ -1,5 +1,6 @@
 import numpy as np
 from ineqpy import inequality
+import pytest
 
 
 def test_gini_2d():
@@ -82,19 +83,46 @@ def test_theil_1d_1_w():
     expected = 0
     assert obtained==expected
 
+def test_ratio_eqaulity():
+    x = np.array([1, 9])
+    w = np.array([9, 1])
+    obtained = inequality.ratio_top_rest(income=x, weights=w)
+    assert obtained == 1.0
+
 def test_ratio_1d():
     x = np.array([57, 63, 81, 79, 88, 57, 42, 3, 77, 89])
     w = np.array([2, 5, 2, 9, 5, 7, 4, 5, 9, 9])
     obtained = inequality.ratio_top_rest(income=x, weights=w)
-    expected = 0.2654955253563142
+    expected = pytest.approx(0.162606577344702)
     assert obtained == expected
 
 def test_ratio_2d():
     x = np.array([[57], [63], [81], [79], [88], [57], [42], [3], [77], [89]])
     w = np.array([[2], [5], [2], [9], [5], [7], [4], [5], [9], [9]])
     obtained = inequality.ratio_top_rest(income=x, weights=w)
-    expected = 0.2654955253563142
+    expected = pytest.approx(0.162606577344702)
     assert obtained == expected
+
+
+@pytest.mark.parametrize('n', range(15, 20))
+def test_ratio_weighted_eq_unweighted(n):
+    # Generating a random list of between 10 and 100 items
+    x = np.random.randint(1, 100, n)
+    w = np.random.randint(1, 5, n)
+
+    # Weight should be the same as repeating the number multiple times
+    xw = []
+    for xi, wi in zip(x,w):
+        xw += [xi]*wi  # Create a list that contains
+    
+    xw = np.array(xw)
+
+    assert len(xw) == np.sum(w)
+    print("n:", n)
+
+    weighted = inequality.ratio_top_rest(income=x, weights=w)
+    unweighted = inequality.ratio_top_rest(income=xw)
+    assert weighted == unweighted
 
 def test_ratio_unweighted():
     x = np.array([
@@ -103,22 +131,7 @@ def test_ratio_unweighted():
        39, 73, 63,  3, 49, 52, 94,  0, 68, 86, 42, 84, 58,  5, 45, 62, 49,
        97, 77, 94, 66, 84, 42, 39,  7, 24, 65, 52, 59, 52, 38, 27, 85, 43,
        26,  6, 93, 24, 48, 42, 50, 58, 89, 79, 94, 50,  2, 46, 82, 98, 69,
-        9, 50, 33, 86, 77, 25, 39, 61, 78, 47, 29, 43, 20, 56, 35])
+       9,  50, 33, 86, 77, 25, 39, 61, 78, 47, 29, 43, 20, 56, 35])
     obtained = inequality.ratio_top_rest(x)
     expected = 0.22203712517848642
-    assert obtained == expected
-
-def test_ratio_1d_0_w():
-    x = np.array([2,       2])
-    w = np.array([1000000, 1])
-    obtained = inequality.ratio_top_rest(income=x, weights=w)
-    expected = 2000000 / 2
-    assert obtained == expected
-
-
-def test_ratio_1d_0_series():
-    x = np.array([2, 2])
-    # w = np.array([1, 1])
-    obtained = inequality.ratio_top_rest(income=x)
-    expected = 1
     assert obtained == expected
